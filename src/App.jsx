41fd0a8c6139f6ae88bef276b52d016aa25abb16
松本省吾 <matsush.oooo12@icloud.com>
2023-10-16 17:27:58 +0900

    test ar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ARButton, XR, Controllers } from "@react-three/xr";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    // <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+    //   <color attach="background" args={["#ececec"]} />
+    //   <Experience />
+    // </Canvas>
+    <>
+      <ARButton />
+      <Canvas>
+        <XR>
+          <Controllers />
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
   );
 }
 
