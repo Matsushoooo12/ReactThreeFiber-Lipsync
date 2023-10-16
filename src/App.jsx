f58baebc7ad4cd09f865ceb21f4d1f603cdaae5a
松本省吾 <matsush.oooo12@icloud.com>
2023-10-16 18:09:28 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-import { ARButton, XR, Controllers } from "@react-three/xr";
 
 function App() {
   return (
-    // <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-    //   <color attach="background" args={["#ececec"]} />
-    //   <Experience />
-    // </Canvas>
-    <>
-      <ARButton />
-      <Canvas>
-        <XR>
-          <Controllers />
-          <mesh>
-            <boxGeometry />
-            <meshBasicMaterial color="blue" />
-          </mesh>
-        </XR>
-      </Canvas>
-    </>
+    <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+      <color attach="background" args={["#ececec"]} />
+      <Experience />
+    </Canvas>
   );
 }
 
